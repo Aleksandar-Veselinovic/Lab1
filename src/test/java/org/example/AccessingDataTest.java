ca0ce9497f31ccc009861fb42d782a2677976d58
Aleksandar Veselinovic <vesa.2k@gmail.com>
2023-02-08 17:26:48 -0500

    add tests

--- src/test/java/org/example/AccessingDataTest.java
+++ src/test/java/org/example/AccessingDataTest.java
@@ -2,47 +2,55 @@ package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AccessingDataTest {
-    @Value("${local.server.port}")
+    @Value(value="${local.server.port}")
     private int port;
 
     @Autowired
     private AddressBookController bookController;
 
+    @Autowired
     private TestRestTemplate template;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @Test
-    public void contextLoads() {
-    }
 
     @Test
     public void testCreateAddressBook() {
-        System.out.println(port);
-        template = new TestRestTemplate();
         JSONObject obj = new JSONObject();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(obj.toString(), headers);
-        String url = "http://localhost:8080/addressBooks";
-        String book = template.postForObject(url, request, String.class);
+        String url = "http://localhost:" + port + "/addressBooks";
+        String response = template.postForObject(url, request, String.class);
 
-        System.out.println(book);
+        assert(response.contains("addressBooks/1"));
+
+        url = "http://localhost:" + port + "/buddies?id=1&name=Bob&number=567";
+        response = template.postForObject(url, request, String.class);
+
+        assert(response.contains("Bob"));
+        assert(response.contains("567"));
+
+        url = "http://localhost:" + port + "/buddyInfoes";
+        response = template.getForObject(url, String.class);
+        assert(response.contains("Bob"));
+        assert(response.contains("567"));
+
+        url = "http://localhost:" + port + "/addressBooks/1/buddies/1";
+        template.delete(url);
+
+        url = "http://localhost:" + port + "/addressBooks/1/buddies";
+        response = template.getForObject(url, String.class);
+        assert(!response.contains("Bob"));
+        assert(!response.contains("567"));
     }
 }
